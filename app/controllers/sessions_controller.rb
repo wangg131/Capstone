530064c7d2463472fece40100981ff727ac592ef
Loren Wang <wang131.131@gmail.com>
2015-10-15 12:46:07 -0700

    routes to account page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to account_path(@user.id)
     else
       flash.now[:login_error] = "Incorrect password. Try Again."
       render :new
@@ -15,7 +15,6 @@ class SessionsController < ApplicationController
   def facebook_user?
    provider != nil && uid != nil
   end
-
 
   def facebook_create
     auth_hash = request.env['omniauth.auth']
