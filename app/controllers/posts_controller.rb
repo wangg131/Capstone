d9304603c981332cf6ad80e9379d23e2c9b53f7c
Loren Wang <wang131.131@gmail.com>
2015-10-16 10:48:07 -0700

    post controller new, create, and params setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,22 @@
 class PostsController < ApplicationController
-  def new; end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(create_params)
+  end
+
+
+
+  private
+
+  def create_params
+    params.require(:post).permit(:name, :email, :password, :password_confirmation, :phone_number, :street, :city, :state, :zip, :user_type, :country_code, :authy_id)
+  end
+
+
+
+
+
 end
