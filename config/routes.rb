e27210dcb09abbc66bf1e378e03666e3c35ce09c
Loren Wang <wang131.131@gmail.com>
2015-10-16 01:50:11 -0700

    fixed routes so more specific ones are at the top...

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root    'home#index'
 
   get     "/signup" => 'users#new', as: 'signup'
 
