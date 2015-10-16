a54b2ba747bbdaa743ec9c4fe9cc7ef2d4152e56
Loren Wang <wang131.131@gmail.com>
2015-10-16 12:28:42 -0700

    should be done figuring out table stuff and params

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,6 +10,6 @@ class ProfilesController < ApplicationController
   private
 
   def create_params
-    params.require(:post).permit(:title, :house_type, :description, :neighborhoods, :rooms_wanted, :bathroom_type, :min_price, :max_price, :cats, :dogs, :parking, :laundry, :max_roommates, :smoking, :gender_preference, :date_needed, :user_id)
+    params.require(:post).permit(:title, :house_type, :description, :neighborhoods, :rooms_needed, :bathroom_type, :min_price, :max_price, :cats, :dogs, :parking, :laundry, :max_roommates, :smoking, :gender_preference, :date_needed, :user_id)
   end
 end
