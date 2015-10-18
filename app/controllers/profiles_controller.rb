7e5eb64b228d0b2135b822a0aad311d1d8363680
Loren Wang <wang131.131@gmail.com>
2015-10-17 20:55:40 -0700

    DRYing up neighborhood selects

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,20 +1,26 @@
 class ProfilesController < ApplicationController
-  CENTRAL_NEIGHBORHOODS = [["Magnolia/Interbay", "Magnolia/Interbay"],
-  ["Queen Anne", "Queen Anne"], ["South Lake Union", "South Lake Union"], ["Montlake/Eastlake", "Montlake/Eastlake"],
-  ["Madison Park/Madison Valley", "Madison Park/Madison Valley"], ["Madrona/Leschi", "Madrona/Leschi"],
-  ["First Hill", "First Hill"], ["Capitol Hill", "Capitol Hill"], ["Central District", "Central District"],
-  ["International District", "International District"], ["Belltown/Denny Triangle/Downtown", "Belltown/Denny Triangle/Downtown"], ["Pioneer Square", "Pioneer Square"]]
+
+  CENTRAL_SEATTLE_SELECT = [["Magnolia/Interbay", "Magnolia/Interbay"],["Queen Anne", "Queen Anne"],
+  ["South Lake Union", "South Lake Union"], ["Montlake/Eastlake", "Montlake/Eastlake"],
+  ["Madison Park/Madison Valley", "Madison Park/Madison Valley"],
+  ["Madrona/Leschi", "Madrona/Leschi"], ["First Hill", "First Hill"], ["Capitol Hill", "Capitol Hill"],
+  ["Central District", "Central District"], ["International District", "International District"],
+  ["Belltown/Denny Triangle/Downtown", "Belltown/Denny Triangle/Downtown"], ["Pioneer Square", "Pioneer Square"]]
+
   def new
     @profile = Profile.new
+    @central_neighborhoods = CENTRAL_SEATTLE_SELECT.each {|neighborhood| neighborhood}
   end
 
   def create
-    @profile = Profile.create(create_params)
+    @profile = Profile.create(profile_params)
   end
 
   private
 
   def create_params
-    params.require(:post).permit(:title, :house_type, :description, :neighborhoods, :rooms_needed, :bathroom_type, :min_price, :max_price, :cats, :dogs, :parking, :laundry, :max_roommates, :smoking, :gender_preference, :date_needed, :user_id)
+    params.require(:post).permit(:title, :house_type, :description, :neighborhoods, :rooms_needed, :bathroom_type,
+    :min_price, :max_price, :cats, :dogs, :parking, :laundry, :max_roommates, :smoking, :gender_preference,
+    :date_needed, :user_id)
   end
 end
