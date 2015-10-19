2e9c0db567efefc6e59c733001364bd37e8fce3f
Loren Wang <wang131.131@gmail.com>
2015-10-19 10:35:47 -0700

    more form work

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,32 +2,30 @@
 
 # profiles constants
   PROFILE_SEATTLE_SELECT =
-  [
-   ['North Seattle',
-     [
+    [
+     ['North Seattle',
        ["Phinney Ridge"], ["Greenwood/Crown Hill"],
        ["Haller Lake/Bitter Lake/Broadview"], ["Ballard/Fremont"],
        ["Greenlake"], ["Wallingford"], ["Northgate/Maple Leaf"],
        ["Lake City"], ["University District"], ["Roosevelt/Ravenna"],
        ["Windermere/Laurelhurst"], ["Wedgwood/Sandpoint"]
-    ]],
-   ['Central Seattle',
-     [
+      ],
+     ['Central Seattle',
        ["Magnolia/Interbay"],["Queen Anne"],
        ["South Lake Union"], ["Montlake/Eastlake"],
        ["Madison Park/Madison Valley"], ["Madrona/Leschi"],
        ["First Hill"], ["Capitol Hill"], ["Central District"],
        ["International District"], ["Belltown/Denny Triangle/Downtown"],
        ["Pioneer Square"]
-     ]],
-  ['South Seattle',
-    [
+       ],
+    ['South Seattle',
       ["Mount Baker"], ["Beacon Hill"], ["Georgetown"],
       ["SODO/Industrial District"], ["Columbia City/Seward Park"],
       ["Rainier Valley"], ["Rainier Beach"], ["South Park"],
       ["Alki"], ["West Seattle"]
-    ]]
-  ]
+      ]
+    ]
+
   HOUSING_TYPES =
-  {"House"=> "House", "Apartment" => "Apartment", "Duplex" => "Duplex", "Townhouse" => "Townhouse",
-    "Condo" => "Condo", "In-Law" => "In-Law"}
+    {"House"=> "House", "Apartment" => "Apartment", "Duplex" => "Duplex",
+      "Townhouse" => "Townhouse", "Condo" => "Condo", "In-Law" => "In-Law"}
