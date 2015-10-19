9ee7735db3621e1a9a6d5e40f132085ee10c6b5a
Loren Wang <wang131.131@gmail.com>
2015-10-19 01:53:02 -0700

    more form wip

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -5,29 +5,29 @@
   [
    ['North Seattle',
      [
-       ["Phinney Ridge", "Phinney Ridge"], ["Greenwood/Crown Hill", "Greenwood/Crown Hill"],
-       ["Haller Lake/Bitter Lake/Broadview"], ["Haller Lake/Bitter Lake/Broadview"], ["Ballard/Fremont", "Ballard/Fremont"],
-       ["Greenlake", "Greenlake"], ["Wallingford", "Wallingford"], ["Northgate/Maple Leaf", "Northgate/Maple Leaf"],
-       ["Lake City", "Lake City"], ["University District", "University District"], ["Roosevelt/Ravenna", "Roosevelt/Ravenna"],
-       ["Windermere/Laurelhurst", "Windermere/Laurelhurst"], ["Wedgwood/Sandpoint", "Wedgwood/Sandpoint"]
+       ["Phinney Ridge"], ["Greenwood/Crown Hill"],
+       ["Haller Lake/Bitter Lake/Broadview"], ["Ballard/Fremont"],
+       ["Greenlake"], ["Wallingford"], ["Northgate/Maple Leaf"],
+       ["Lake City"], ["University District"], ["Roosevelt/Ravenna"],
+       ["Windermere/Laurelhurst"], ["Wedgwood/Sandpoint"]
     ]],
    ['Central Seattle',
      [
-       ["Magnolia/Interbay", "Magnolia/Interbay"],["Queen Anne", "Queen Anne"],
-       ["South Lake Union", "South Lake Union"], ["Montlake/Eastlake", "Montlake/Eastlake"],
-       ["Madison Park/Madison Valley", "Madison Park/Madison Valley"], ["Madrona/Leschi", "Madrona/Leschi"],
-       ["First Hill", "First Hill"], ["Capitol Hill", "Capitol Hill"], ["Central District", "Central District"],
-       ["International District", "International District"], ["Belltown/Denny Triangle/Downtown", "Belltown/Denny Triangle/Downtown"],
-       ["Pioneer Square", "Pioneer Square"]
+       ["Magnolia/Interbay"],["Queen Anne"],
+       ["South Lake Union"], ["Montlake/Eastlake"],
+       ["Madison Park/Madison Valley"], ["Madrona/Leschi"],
+       ["First Hill"], ["Capitol Hill"], ["Central District"],
+       ["International District"], ["Belltown/Denny Triangle/Downtown"],
+       ["Pioneer Square"]
      ]],
   ['South Seattle',
     [
-      ["Mount Baker", "Mount Baker"], ["Beacon Hill", "Beacon Hill"], ["Georgetown", "Georgetown"],
-      ["SODO/Industrial District", "SODO/Industrial District"], ["Columbia City/Seward Park", "Columbia City/Seward Park"],
-      ["Rainier Valley", "Rainier Valley"], ["Rainier Beach", "Rainier Beach"], ["South Park", "South Park"],
-      ["Alki", "Alki"], ["West Seattle", "West Seattle"]
+      ["Mount Baker"], ["Beacon Hill"], ["Georgetown"],
+      ["SODO/Industrial District"], ["Columbia City/Seward Park"],
+      ["Rainier Valley"], ["Rainier Beach"], ["South Park"],
+      ["Alki"], ["West Seattle"]
     ]]
   ]
   HOUSING_TYPES =
   {"House"=> "House", "Apartment" => "Apartment", "Duplex" => "Duplex", "Townhouse" => "Townhouse",
-    "condo" => "Condo", "in-law" => "in-law"}
+    "Condo" => "Condo", "In-Law" => "In-Law"}
