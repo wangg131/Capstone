ffe056d4451bc0f1e849b8701731be6b274d023b
Loren Wang <wang131.131@gmail.com>
2015-10-20 12:50:29 -0700

    user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -13,19 +13,4 @@ FactoryGirl.define do
     post_id                 nil
     profile_id              2
   end
-
-  # factory :user_host do
-  #   id                      6
-  #   name                    "Tianna"
-  #   email                   "shoo@shoo.com"
-  #   password                "shoo"
-  #   password_confirmation   "shoooo"
-  #   password_digest         "rlksdjfs"
-  #   phone_number            3604216650
-  #   country_code            1
-  #   user_type               "host"
-  #   authy_id                567887
-  #   post_id                 1
-  #   profile_id              nil
-  # end
 end
