295ae6e1c6d8b72ae4cc8a1cf3a8319d59c82bbb
Loren Wang <wang131.131@gmail.com>
2015-10-20 00:36:12 -0700

    messages and conversations controllers/models setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020013812) do
+ActiveRecord::Schema.define(version: 20151020072707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text    "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.boolean "read",            default: false
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at",        null: false
