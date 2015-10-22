ae1c73567b4d7ad45b0827a9f5e9cc16f4b52b7e
Loren Wang <wang131.131@gmail.com>
2015-10-21 20:59:06 -0700

    more config?

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    # Configuration for Amazon S3 should be made available through an Environment variable.
-    # For local installations, export the env variable through the shell OR
-    # if using Passenger, set an Apache environment variable.
-    #
     # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
 
     # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret
