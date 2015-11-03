ed52438aab0c3df3ba1965d1a7e8bd56e91f9acb
Loren Wang <wang131.131@gmail.com>
2015-11-02 16:18:53 -0800

    carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
   config.aws_credentials  = {
     access_key_id:          ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key:      ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                 'us-west-2'
+    region:                 ENV['S3_REGION']
     }
 
     # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
